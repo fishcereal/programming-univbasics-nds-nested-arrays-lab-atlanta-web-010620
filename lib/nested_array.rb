9a1f30caf276eb888f1c6d4117104ab6ab11db1b
agile-prototype-8421 <tylergreason@gmail.com>
2019-12-05 16:30:08 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -40,6 +40,6 @@ end
 def matrix_update(matrix, row, column, new_value)
   # Update the matrix location at row and column to have the value of new_value
   # Return the updated matrix
-  matrix[row][column]=new_value
-  return matrix
+  updated_matrix[row][column]=new_value
+  return updated_matrix
 end
